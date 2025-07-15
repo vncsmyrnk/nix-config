695f1cb0335f002ffe30788b47bfb6cef5b75702
Vinicius Mayrink <vmayrink12@gmail.com>
2025-07-15 08:36:18 -0300

    chore(nixpkgs): pins go version at 1.24.5 for now

--- nixpkgs/packages.nix
+++ nixpkgs/packages.nix
@@ -1,4 +1,14 @@
 with import <nixpkgs> {}; [
+  (go.overrideAttrs (oldAttrs: rec {
+    version = "1.24.5";
+    src = fetchFromGitHub {
+      owner = "golang";
+      repo = "go";
+      rev = "go${version}";
+      sha256 = "sha256-/KvCdXaE92t/LRbUTHkPa+MhuA0lvhSUqxC237imEy0=";
+    };
+  }))
+
   tmux
   neovim
   lazygit
@@ -8,7 +18,6 @@ with import <nixpkgs> {}; [
   rclone
   just
   mkcert
-  go
   fluxcd
   lua51Packages.luarocks
   nodejs
