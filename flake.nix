f90b75ad03d601def66283ea346ce7e668481b69
Vinicius Mayrink <vmayrink12@gmail.com>
2025-08-25 14:14:20 -0300

    chore(flake): removes golang version pinning
    1.25 is out now, no need for pinning.
    
    I pinned it in the first place to avoid getting rc versions.

--- flake.nix
+++ flake.nix
@@ -10,19 +10,6 @@
       system = "x86_64-linux";
       pkgs = import nixpkgs {
         inherit system;
-        overlays = [
-          (final: prev: {
-            go = prev.go.overrideAttrs (oldAttrs: rec {
-              version = "1.24.5";
-              src = prev.fetchFromGitHub {
-                owner = "golang";
-                repo = "go";
-                rev = "go${version}";
-                hash = "sha256-/KvCdXaE92t/LRbUTHkPa+MhuA0lvhSUqxC237imEy0=";
-              };
-            });
-          })
-        ];
       };
     in
     {
